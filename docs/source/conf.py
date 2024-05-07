0ada8871bd506504f0af46501d7a9ae862ea4e7e
Esden <57993536+EsdenRun@users.noreply.github.com>
2024-05-07 15:58:45 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SpaGNNs'
+copyright = '2024, ChangXu'
+author = 'ChangXu'
 
 release = '0.1'
 version = '0.1.0'
